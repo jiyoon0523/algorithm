b1358a3a1efb181908208e043c3503d456c450ff
Jiyoon <jylindsay0523@gmail.com>
2023-02-26 23:21:58 +0900

    1_Programmers_Lv1_YearOf2016

---
+++ Programmers/Programmers/YearOf2016.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Programmers
+{
+    public class YearOf2016
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(1, 1));
+            Console.WriteLine(Solution(5, 24));
+        }
+
+        static string[] dayArray = new string[] { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+        static int[] dateArray = new int[] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        public static string Solution(int a, int b)
+        {
+            int days = 0;
+            for (int i = 0; i < a - 1; i++)
+            {
+                days += dateArray[i];
+            }
+            days += b;
+            days -= 1;
+
+            var fromFri = days % 7;
+            string answer = dayArray[fromFri];
+
+            return answer;
+        }
+    }
+}
